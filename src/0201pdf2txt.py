a6901d00f6bb13dfe5d1dab6876bd57feb90edba
huangbin2005 <huangbin2005@gmail.com>
2024-05-06 09:03:12 +0800

    每日提交

--- src/0201pdf2txt.py
+++ src/0201pdf2txt.py
@@ -1,37 +1,72 @@
 # coding=utf-8
 __author__ = 'laohuang'
 
-from paddleocr import PaddleOCR
+# from paddleocr import PaddleOCR
 import pdf2image
 import os
-ocr = PaddleOCR(use_angle_cls=True, lang='ch')  # 中文设置lang参数为'ch'
+import multiprocessing
+
+# ocr = PaddleOCR(use_angle_cls=True, lang='ch')  # 中文设置lang参数为'ch'
 
 # 将PDF转换为图片
 def convert_pdf_to_images(pdf_path, output_folder):
     try:
         pdf2image.convert_from_path(pdf_path, output_folder=output_folder, fmt='png')
-    except e:
-        print("******",e)
+    except:
+        print("******")
+
+
+def extract_text_from_image(returntxt,image_path,output_text_file):
+    from paddleocr import PaddleOCR
+    ocr = PaddleOCR(use_angle_cls=True, lang='ch')  # 中文设置lang参数为'ch'
+    result = ocr.ocr(image_path, cls=True)
+    returntxt.put(result)
+    with open(output_text_file, 'a+', encoding='utf-8') as f:
+        for line in result:
+            print(line)
+            try:
+                for l1 in line:                 
+                    try:
+                        print(l1[1][0]+'\n')
+                        f.write(l1[1][0]+'\n')
+                    except e:
+                        print("=====",e)
+            except:
+                print("-----------")
+
+
 
 # 从图片中提取文本
 def extract_text_from_images(image_folder, output_text_file):
-    
-    with open(output_text_file, 'w', encoding='utf-8') as f:
+    # with open(output_text_file, 'w', encoding='utf-8') as f:
         for image_file in os.listdir(image_folder):
             if image_file.endswith('.png'):
                 image_path = os.path.join(image_folder, image_file)
-                result = ocr.ocr(image_path, cls=True)
-                for line in result:
-                    print(line)
-                    try:
-                        for l1 in line:                 
-                            try:
-                                print(l1[1][0]+'\n')
-                                f.write(l1[1][0]+'\n')
-                            except e:
-                                print("=====",e)
-                    except:
-                        print("-----------")
+                print("image_folder======>",image_folder)
+                print("image_file======>",image_file)
+                print("image_path======>",image_path)
+                q = multiprocessing.Queue()
+                p = multiprocessing.Process(target=extract_text_from_image, args=(q, image_path,output_text_file))
+                p.start()
+                p.join()
+                if not q.empty():
+                    result = q.get()
+                    print("Result:", result) 
+                else:
+                    print("No result found")
+
+                # result = ocr.ocr(image_path, cls=True)
+                # for line in result:
+                #     print(line)
+                #     try:
+                #         for l1 in line:                 
+                #             try:
+                #                 print(l1[1][0]+'\n')
+                #                 f.write(l1[1][0]+'\n')
+                #             except e:
+                #                 print("=====",e)
+                #     except:
+                #         print("-----------")
 # 将文件的目录
 def initpicpath(output_folder):
     # 
@@ -55,7 +90,7 @@ def traverse_directory(directory):
         for file in files:
             file_path = os.path.join(root, file)
             print(file_path)  # 这里可以根据需求对文件路径进行处理
-            image_folder = '/mnt/d/资料/pdf_images'  # 图片输出文件夹
+            image_folder = '/mnt/d/information/pdf_images'  # 图片输出文件夹
             initpicpath(image_folder)
             convert_pdf_to_images(file_path, output_folder=image_folder)
             extract_text_from_images(image_folder, file_path+".process.txt")
@@ -65,7 +100,7 @@ def main():
     # initpicpath(image_folder)
     # convert_pdf_to_images(pdf_path, output_folder=image_folder)
     # extract_text_from_images(image_folder, output_text_file)
-    pdfs_path = '/mnt/d/资料/和君书单'  # 替换为你的PDF文件路径
+    pdfs_path = '/mnt/d/information/和君书单'  # 替换为你的PDF文件路径
     traverse_directory(pdfs_path)
 
 if __name__ == '__main__':
